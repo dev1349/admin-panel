703c168eaa88464f6c2182a99a8d22807b5ddf99
Туровский Ярослав <turovskyiys0011@gmail.com>
2021-12-21 09:15:35 +0000

    Merged in feature/WSADMIN-86(fix) (pull request #55)
    bug fix of WSADMIN-86
    
    * -bug fix
    
    
    Approved-by: Oleksiy Kyrychenko
    Approved-by: Sergey Bogdanov
    Approved-by: Тарас Бондаренко

--- src/theme.js
+++ src/theme.js
@@ -628,6 +628,7 @@ const globalTheme = createTheme({
                     width: '100%',
                     paddingLeft: '20px',
                     '@media (min-width: 1500px)': {
+                        marginLeft: '50px',
                         width: '15%',
                     },
                 },
