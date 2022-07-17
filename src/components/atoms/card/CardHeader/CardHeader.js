8c14cb8a1db41591ac6dd1071b789293d0634b80
Борсук Валентин <b1rsu_k@protonmail.ch>
2022-07-17 13:16:05 +0000

    Merged in feature/WSADMIN-135 (pull request #93)
    Feature/WSADMIN-135
    
    * card
    
    * header
    
    * WSADMIN-135/final
    
    * fix/WSADMIN-135
    
    * fix/WSADMIN-135
    
    * final
    
    * array-props
    
    * bagfix
    
    * Merge remote-tracking branch 'origin/develop' into feature/WSADMIN-135
    
    # Conflicts:
    #	src/components/atoms/card/CardHeader/CardHeader.js

--- src/components/atoms/card/CardHeader/CardHeader.js
+++ src/components/atoms/card/CardHeader/CardHeader.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { createStyled } from '../../../../services/style/createStyled'
 
-const CardHeaderStyled = createStyled('CardHeaderStyle', 'header', ['green', 'black', 'red'])
+const CardHeaderStyled = createStyled('CardHeaderStyle', 'header', ['positiveProgress', 'negativeProgress', 'neutralProgress'])
 
 const CardHeader = ({ children, progress }) => {
     return (
-        <CardHeaderStyled positiveProgress={progress > 0} negativeProgress={progress < 0} neutalProgress={progress === 0}>
+        <CardHeaderStyled positiveProgress={progress > 0} negativeProgress={progress < 0} neutralProgress={progress === 0}>
             {children}
         </CardHeaderStyled>
     )
